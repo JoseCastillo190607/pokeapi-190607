6e7ba868c2b987e823bb409e8d68c5a77c41a589
Jose Castillo <josegzcasz@gmail.com>
2022-07-10 21:19:31 -0500

    feact: adde navigation, added link of img pokeball for the search page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect} from 'react'
-import {Typography,Paper, Container, Card, CardActionArea, CardContent, CardMedia, Grid } from '@material-ui/core'
-
+import React, { useState, useEffect } from 'react'
+import { Typography, Paper, Container, Card, CardActionArea, CardContent, CardMedia, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import axios from 'axios'
 import './layout.css'
 import pokeball from '../img/pokeball.png'
+import { Link } from 'react-router-dom'
 
 
 const useStyles = makeStyles(theme => ({
@@ -35,128 +35,130 @@ const useStyles = makeStyles(theme => ({
 	},
 	imagePoke: {
 		height: 250,
-		Justify:'center',
+		Justify: 'center',
 	}
 }))
 
 const Layout = () => {
 
-const classes = useStyles()
-const [pokemon, setPokeNum] = useState(0);
-const [pokedata, setPokeData] = useState("");
-const [pokedataesp, setPokeDataESP] = useState([]);
+	const classes = useStyles()
+	const [pokemon, setPokeNum] = useState(0);
+	const [pokedata, setPokeData] = useState("");
+	const [pokedataesp, setPokeDataESP] = useState([]);
 
 
 
 
-const getdata = () => {
-  axios
-	.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${pokemon}`)
-	.then((res) => {
-	  setPokeData(res.data.results);
-	});
-};
+	const getdata = () => {
+		axios
+			.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${pokemon}`)
+			.then((res) => {
+				setPokeData(res.data.results);
+			});
+	};
 
-useEffect(getdata, [pokemon]);
+	useEffect(getdata, [pokemon]);
 
-useEffect(() => {
-  setPokeDataESP([]);
-  if (pokedata.length >= 1) {
-	pokedata.map((item) => {
-	  axios.get(`${item.url}`).then((data) => {
-		setPokeDataESP((current) => [...current, { data: data.data }]);
-		console.log(data.data);
-		if (pokedataesp >= 10) {
-		  return pokedataesp;
+	useEffect(() => {
+		setPokeDataESP([]);
+		if (pokedata.length >= 1) {
+			pokedata.map((item) => {
+				axios.get(`${item.url}`).then((data) => {
+					setPokeDataESP((current) => [...current, { data: data.data }]);
+					console.log(data.data);
+					if (pokedataesp >= 10) {
+						return pokedataesp;
+					}
+				});
+			});
 		}
-	  });
-	});
-  }
-}, [pokedata]);
+	}, [pokedata]);
 
 
 	return (
 		<div className='pokemonContenedor'>
-		<div className="pokedex-container">
-        <div className="pokedex__header">
-            <div className="pokedex__header-title">
-                <h1>POKEDEX 190607</h1>
-            </div>
-           
-            
-            <div className="pokedex__header-aline">
-                <div className="arrow-leftContent">
-        
+			<div className="pokedex-container">
+				<div className="pokedex__header">
+					<div className="pokedex__header-title">
+						<h1>POKEDEX 190607</h1>
+					</div>
+
+
+					<div className="pokedex__header-aline">
+						<div className="arrow-leftContent">
+
+						</div>
+						<input
+							className="arrow-left"
+							type="button"
+							value="Pagina Anterior"
+							onClick={(event) => {
+								if (pokemon <= 0) {
+									setPokeNum(Number(1116));
+								} else {
+									setPokeNum(Number(pokemon) - 10);
+								}
+							}}
+						/>
+						<div className="arrow-rightContent" >
+
+						</div>
+
+						<div className="pokedex__header-search">
+							<form action="" id="searchPokemon">
+								<Link to='/Busqueda' className='pokeImg' > 
+									<img src={pokeball} alt="pokeball" className='pokeImg' />
+								</Link>
+							</form>
+						</div>
+
+						<input
+							className="arrow-right"
+							type="button"
+							value="Pagina Siguiente"
+							onClick={(event) => {
+								if (pokemon >= 1125) {
+									setPokeNum(Number(0));
+								} else {
+									setPokeNum(Number(pokemon) + 10);
+								}
+							}}
+						/>
+					</div>
 				</div>
-        <input
-          className="arrow-left"
-          type="button"
-          value="Pagina Anterior"
-          onClick={(event) => {
-            if (pokemon <= 0) {
-              setPokeNum(Number(1116));
-            } else {
-              setPokeNum(Number(pokemon) - 10);
-            }
-          }}
-        />
-                <div className="arrow-rightContent" >
-         
-          </div>  
+			</div>
 
-		  <div className="pokedex__header-search">
-                <form action="" id="searchPokemon">
-                        <img src={pokeball} alt="pokeball"/>
-                </form>
-            </div>
 
-             <input
-          className="arrow-right"
-          type="button"
-          value="Pagina Siguiente"
-          onClick={(event) => {
-            if (pokemon >= 1125) {
-              setPokeNum(Number(0));
-            } else {
-              setPokeNum(Number(pokemon) + 10);
-            }
-          }}
-        />
-            </div>
-        </div>
-    </div>
-	
+			<div className="text-center">
 
-			  <div className="text-center">
-      
-	  <Container maxWidth='lg' component={Paper} elevattion={4} className={classes.container}>
-       <Card container spacing={2}>
-		<Grid container spacing={2} justify='center' >
-          {pokedataesp.map((poke, index) => (
-            <Grid item xs={12} sm={4} key={index}>
-			<CardActionArea>
-				<CardContent>
-					<CardMedia >
-               			 <img justify='center'
-                 			src={
-                    
-                    		    poke.data.sprites.front_default
-                 	 		}
-							  className={classes.imagePoke}
-                  			alt="No image"
-				  		/>
-			
-					</CardMedia>  
-					<Typography align='center' variant='h4'>{`${poke.data.id}`}</Typography>
-					<Typography align='center' variant='h4'>{`${poke.data.name}`}</Typography>
-				</CardContent>
-			</CardActionArea>
-        </Grid>
-          ))}
-        </Grid>
-		</Card>
-      </Container>
-	  </div>
+				<Container maxWidth='lg' component={Paper} elevattion={4} className={classes.container}>
+					<Card container spacing={2}>
+						<Grid container spacing={2} justify='center' >
+							{pokedataesp.map((poke, index) => (
+								<Grid item xs={12} sm={4} key={index}>
+									<CardActionArea>
+										<CardContent>
+											<CardMedia >
+												<img justify='center'
+													src={
+
+														poke.data.sprites.front_default
+													}
+													className={classes.imagePoke}
+													alt="No image"
+												/>
+
+											</CardMedia>
+											<Typography align='center' variant='h4'>{`${poke.data.id}`}</Typography>
+											<Typography align='center' variant='h4'>{`${poke.data.name}`}</Typography>
+										</CardContent>
+									</CardActionArea>
+								</Grid>
+							))}
+						</Grid>
+					</Card>
+				</Container>
+			</div>
 		</div>
 	)
 }
